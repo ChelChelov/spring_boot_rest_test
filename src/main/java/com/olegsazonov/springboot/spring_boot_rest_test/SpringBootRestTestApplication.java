f31dfd836f7e57e94e2663c61958e3a676975320
Oleg Sazonov <sazonov1994@gmail.com>
2022-10-15 00:30:45 +0300

    Created project by Spring Boot

---
+++ src/main/java/com/olegsazonov/springboot/spring_boot_rest_test/SpringBootRestTestApplication.java
@@ -0,0 +1,13 @@
+package com.olegsazonov.springboot.spring_boot_rest_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olegsazonov/springboot/spring_boot_rest_test/SpringBootRestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olegsazonov.springboot.spring_boot_rest_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
