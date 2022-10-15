777fc044c608aa7bd3fd02359b8536efd4d3986b
Oleg Sazonov <sazonov1994@gmail.com>
2022-10-15 15:39:08 +0300

    Added Entity, DAO, Service and Controller

---
+++ src/main/java/com/olegsazonov/springboot/spring_boot_rest_test/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.olegsazonov.springboot.spring_boot_rest_test.dao;
+
+import com.olegsazonov.springboot.spring_boot_rest_test.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> getAllEmployees();
+
+//    void saveEmployee(Employee employee);
+//
+//    Employee getEmployee(int id);
+//
+//    void deleteEmployee(int id);
+}
